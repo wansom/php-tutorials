8aeefa09df5ff276b478b706b4dab4ae71115732
wansom <ochiengwarren10@gmail.com>
2022-08-02 08:58:13 +0300

    VATRIABLES

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
+//strings
 $user_name ="warren";
 $phone_number="+254705122230";
 echo($user_name);
+//intagers &floats
+$net_salary = 200000;
+$gross_salary = 270000.00;
+echo($net_salary);
+//constants
 define("AGE",26);
 echo AGE;
+//arrays
+$friends =["sang","Abdi","Maroa","Sunday"];
+print_r($friends);
+$family =["sister"=>"Joan","brother"=>"Dancun","Father"=>"Josiah","mother"=>"Margaret"];
+echo($family["sister"]);
+//objects and classes
+
+
 
 ?>
